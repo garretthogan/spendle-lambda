a763e94b98b9e4ecb49cce88d6f0fb69719d1176
Garrett Hogan <garrett.hogan.dev@gmail.com>
2018-05-17 21:40:08 -0700

    scanning all items and sending texts

--- index.js
+++ index.js
@@ -14,26 +14,25 @@ exports.handler = function (event, context) {
 
   const searchParams = {
     TableName: 'spendle-user-data',
-    Key: {
-      userId: 'abc123'
-    }
   };
 
-  dynamoDB.get(searchParams).promise().then(res => {
-    const toNumber = res.Item.phoneNumber;
-    const spentThisMonth = res.Item.spentThisMonth;
-    const target = res.Item.targetSavingsPercentage;
-    const income = res.Item.incomeAfterBills;
-    const remaining = income - spentThisMonth;
-    const budget = remaining * (target / 100);
-    const today = budget / daysRemaining;
-
-    client.messages.create({
-      body: `Hey! Looks like you spent about $${spentThisMonth.toFixed(2)} this month. 
-              \nYou should probably only spend about $${today.toFixed(2)} today. 
-              \nYou have $${remaining.toFixed(2)} remaining in your budget for the month!`,
-      to: `+${toNumber}`,
-      from: '+12252636117'
-    }).then(message => context.succeed(message)).catch(error => context.fail(error));    
+  dynamoDB.scan(searchParams).promise().then(res => {
+    res.Items.forEach(item => {
+      const toNumber = item.phoneNumber;
+      const spentThisMonth = item.spentThisMonth;
+      const target = item.targetSavingsPercentage;
+      const income = item.incomeAfterBills;
+      const remaining = income - spentThisMonth;
+      const budget = (income - (income * (target / 100)) - spentThisMonth);
+      const today = budget / daysRemaining;
+  
+      client.messages.create({
+        body: `Hey! Looks like you spent about $${spentThisMonth.toFixed(2)} this month. 
+                \nYou should probably only spend about $${today.toFixed(2)} today. 
+                \nYou have $${remaining.toFixed(2)} remaining in your budget for the month!`,
+        to: `+${toNumber}`,
+        from: '+12252636117'
+      }).then(message => context.succeed(message)).catch(error => context.fail(error));
+    });
   }).catch(error => context.fail(error));
 }
